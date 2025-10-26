7bc5c3aba9bf605224b8615106e1d479eb909d2f
OscarHealyATU <G00424324@atu.ie>
2025-10-26 01:02:03 +0000

    added error handling for middleware

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send('Welcome to Data Respresentation & Querying')
 })
+// error handling middleware 
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong!');
+});
+
 // starts the server and listens on port 3000
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
