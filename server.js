1091f17c178ddae942fd6b8317bf6f97542ac2a4
OscarHealyATU <G00424324@atu.ie>
2025-10-26 01:56:11 +0000

    Exercise 5: Return JSON Data

--- server.js
+++ server.js
@@ -3,10 +3,49 @@ const app = express()
 const port = 3000
 
 // defines a route for get requests, / is the site root
+/*
 app.get('/', (req, res) => {
-  res.send('Welcome to Data Respresentation & Querying')
-})
-// error handling middleware 
+    res.send('Hello World');
+});
+*/
+app.get('/api/movies', (req, res) => {
+    const movies = [
+        {
+            "Title": "Avengers: Infinity War",
+            "Year": "2018",
+            "imdbID": "tt4154756",
+            "Type": "movie",
+            "Poster": "https://example.com/poster1.jpg"
+        },
+        {
+            "Title": "Captain America: Civil War",
+            "Year": "2016",
+            "imdbID": "tt3498820",
+            "Type": "movie",
+            "Poster": "https://example.com/poster2.jpg"
+        },
+        {
+            "Title": "World War Z",
+            "Year": "2013",
+            "imdbID": "tt0816711",
+            "Type": "movie",
+            "Poster": "https://example.com/poster3.jpg"
+        }
+    ];
+    // sends a JSON response with status code 201
+    res.status(201).json({ myMovies: movies });
+});
+// defines a route with parameters name 
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name;
+    res.send(`Hello ${name}`);
+});
+// defines a route with parameters name and surname
+app.get('/hello/:name/:surname', (req, res) => {
+    const {name,surname} = req.params;
+    res.send(`Hello ${name} ${surname}`);
+});
+// error handling middleware
 app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something went wrong!');
